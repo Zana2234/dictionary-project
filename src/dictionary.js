3c9f450501962b433eed6034f8336cf86b2a2d2e
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-05-08 13:20:05 +0200

    Added API

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import React from "react";
 import "./dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
     alert(`Searching for "${keyword}" definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
   }
   return (
     <div className="Dictionary">
